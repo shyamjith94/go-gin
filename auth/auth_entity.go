958c585ceca81f4daaec64deeac8fee707fd1e9d
shyamjith <shyamjith@zealogic.onmicrosoft.com>
2023-04-06 18:48:28 +0530

    auth system

---
+++ auth/auth_entity.go
@@ -0,0 +1,9 @@
+package auth
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type User struct {
+	Id       primitive.ObjectID `json:"id,omitempty"`
+	Name     string             `json:"name,omitempty" validate:"required"`
+	Location string             `json:"location,omitempty" validate:"required"`
+}
